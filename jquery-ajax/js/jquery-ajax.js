4acc12db6ebdf3037e3cd66331adf92f28a965ab
Kerttu Kamula <veerakek@k-d557-mac-14.pk.amk.root>
2022-09-01 11:16:22 +0300

    minor changes to jquery-ajax exercise

--- jquery-ajax/js/jquery-ajax.js
+++ jquery-ajax/js/jquery-ajax.js
@@ -2,6 +2,9 @@ $('#search-form').submit(function(e) {
     // Prevent form default action
     e.preventDefault();
 
+    // Empty search-result
+    $('#search-result').empty();
+
     const website = $(this).attr('action');
     const searchTerms = $(this).serialize();
     const url = website + '?' + searchTerms;
@@ -9,12 +12,19 @@ $('#search-form').submit(function(e) {
     $.getJSON(url, function(shows) {
         console.log(shows);
         $.each(shows, function(i, show) {
-            const name = show.show.name;
-            const img = show.show.image ? show.show.image.medium : "https://via.placeholder.com/210x295";
-            const summary = show.show.summary;
-            const homesite = show.show.officialSite || show.show.url;
             // const homesite = show.show.officialSite ? show.show.officialSite : show.show.url;
-            $('#search-result').append(`<article><img src="${img}"><h2>${name}</h2>${summary}<a href="${homesite}">Kotisivu</a></article>`);
+            $('#search-result').append(
+                `<article>
+                    <img src="${
+                        show.show.image 
+                            ? show.show.image.medium 
+                            : 'https://static.tvmaze.com/images/no-img/no-img-portrait-text.png'
+                    }" alt="${show.show.name}">
+                    <h2>${show.show.name}</h2>
+                    <a href="${show.show.officialSite || show.show.url}" target="_blank">Kotisivu</a>
+                    <p>${show.show.genres.join(' / ')}</p>
+                    ${show.show.summary}
+                </article>`);
         });
     });
 });
